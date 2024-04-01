08063f8044f42e9c236dc8401ef77b7108920783
Nabin Hamal <nabinhamal2@gmail.com>
2024-04-01 11:13:14 +0545

    loading and signupui page created

---
+++ app/signup.tsx
@@ -0,0 +1,83 @@
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { Link, useRouter } from 'expo-router';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+ 
+} from 'react-native';
+const Page = () => {
+
+
+  const onSignup = async () => {} 
+
+  
+
+  return (
+    
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Let's get started!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number. We will send you a confirmation code there
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Country code"
+            placeholderTextColor={Colors.gray}
+           
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="Mobile number"
+            placeholderTextColor={Colors.gray}
+            keyboardType="numeric"
+           
+          />
+        </View>
+
+        <Link href={'/login'} replace asChild>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>Already have an account? Log in</Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{ flex: 1 }} />
+
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+           
+            { marginBottom: 20 },
+          ]}
+          onPress={onSignup}>
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+  
+  );
+};
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
+export default Page;
